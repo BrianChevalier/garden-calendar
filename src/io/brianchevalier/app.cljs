9aa75d8693a65fcb6d4f05fd0d51568e03b40b66
Brian Chevalier <adambadahdah@gmail.com>
2024-08-28 19:47:39 -0700

    Breakout calendar component

--- src/io/brianchevalier/app.cljs
+++ src/io/brianchevalier/app.cljs
@@ -6,6 +6,7 @@
    [io.brianchevalier.schema]
    [shadow.resource :as shadow.resource]
    [io.brianchevalier.components :as components]
+   [io.brianchevalier.calendar :as calendar]
    [uix.core :as uix :refer [defui $]]))
 
 (defn get-data []
@@ -13,91 +14,6 @@
     (when-not (spec/valid? :plant/db data)
       (throw (ex-info "Schema error" {:data data})))
     data))
-
-(def months
-  [:jan :feb :mar :apr :may :jun :jul :aug :sept :oct :nov :dec])
-
-(def month->int
-  {:jan  0
-   :feb  1
-   :mar  2
-   :apr  3
-   :may  4
-   :jun  5
-   :jul  6
-   :aug  7
-   :sept 8
-   :oct  9
-   :nov  10
-   :dec  11})
-
-(def day->offset
-  {0  0
-   1  0
-   15 1
-   30 2})
-
-(defn ->col
-  [[month day]]
-  (+ 2 (day->offset day)
-     (* 2 (month->int month))))
-
-(defui periods
-  [{:keys [row periods classes]}]
-  ($ :<>
-    (map-indexed (fn [i {:keys [start end]}]
-                   ($ components/div
-                     {:key   i
-                      :classes (concat classes
-                                       [:opacity-70
-                                        :h-5
-                                        :rounded-md
-                                        (str "row-start-" row)
-                                        (str "col-start-" (->col start))
-                                        (str "col-end-" (->col end))])}))
-                 periods)))
-
-(def k->classes
-  {:plant/sow-indoors  [:bg-blue-500]
-   :plant/sow-outdoors [:bg-green-500 :mt-3]
-   :plant/transplant   [:bg-orange-500 :mt-6]})
-
-(defui table-row
-  [{:keys [i plant]}]
-  (let [row  (inc (inc i))
-        name (:plant/name plant)]
-    ($ :<>
-      ($ :div {:class (str "col-start-1 row-start-" row)}
-        ($ :div.flex.flex-col
-          name
-          ($ :i.text-stone-400
-            (:plant/scientific-name plant))))
-      (for [k (keys k->classes)]
-        ($ periods {:key     k
-                    :row     row
-                    :classes (k->classes k)
-                    :periods (get plant k)})))))
-
-(defui table-rows [{:keys [plants]}]
-  (map-indexed (fn [i p]
-                 ($ table-row {:i     i
-                               :key   i
-                               :plant p}))
-               plants))
-
-(defui table-header []
-  ($ :<>
-    (map-indexed (fn [i month]
-                   ($ components/div
-                     {:key   i
-                      :classes [:relative
-                                :text-center
-                                :col-span-2
-                                :row-start-1
-                                (str "col-start-" (inc (inc (* 2 i))))]}
-                     ($ :div
-                       (str/capitalize (name month)))))
-                 months)))
 
 (defn query
   [{plants :plant/plants}
@@ -116,43 +32,6 @@
     ($ :div.text-gray-300.font-bold.text-xl.text-center.my-auto.mx-auto
       "Sonoran Desert Gardening")))
 
-(defui vline
-  "Draw a vertical line on the calendar"
-  [{:keys [color date width]
-    :or {color :bg-slate-600
-         width :w-0.5}}]
-  ($ components/div
-    {:classes [color
-               :rounded
-               width
-               :row-start-2
-               :row-end-26
-               (str "col-start-" (->col date))]}))
-
-(defui gridlines
-  "Draw gridlines on the calendar"
-  []
-  ($ :<>
-    (for [month months
-          day   [0 15]]
-      ($ vline {:key   [month day]
-                :date  [month day]
-                :color :bg-slate-600}))))
-
-(defui current-day
-  "Draw 'today' on the calendar"
-  []
-  (let [now (js/Date.)
-        month (get months (.getMonth now))
-        day ({0 1
-              1 15
-              2 30}
-             (quot (.getDate now) 15))]
-    ($ vline
-      {:color :bg-red-900
-       :width :w-2
-       :date [month day]})))
-
 (defui app
   []
   (let [db                (get-data)
@@ -163,9 +42,4 @@
       ($ :div.flex.flex-col.gap-10.p-5
         ($ components/search {:value     (:search value)
                               :on-change (fn [v] (on-change (assoc value :search v)))})
-        ($ :div.overflow-scroll
-          ($ :div.grid.grid-cols-25.grid-rows-25.grid-flow-row.gap-y-5.min-w-160
-            ($ gridlines)
-            ($ current-day)
-            ($ table-header)
-            ($ table-rows {:plants plants})))))))
+        ($ calendar/calendar {:plants plants})))))
